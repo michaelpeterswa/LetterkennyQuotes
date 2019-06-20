5f48b07857a194fe2b0f9a23d4cfab73339fe14e
michaelpeterswa <michaelpeterswa@icloud.com>
2019-06-19 22:42:10 -0700

    added lk.py

--- lk.py
+++ lk.py
@@ -3,19 +3,19 @@ import random
 import time
 random.seed(time.time())
 
-quotes = ["Fuck you, your lifes so fuckin' miserable I ran a 15k to raise awareness for it",
+quotes = ["Fuck you, your life's so fuckin' miserable I ran a 15k to raise \nawareness for it",
           "Pitter-patter, lets get at 'er.",
-          "You’re asking the important questions and that’s what I appreciates about you.",
+          "You’re asking the important questions and that’s what I appreciates \nabout you.",
           "Get this guy a fucking puppers.",
           "Big city slams, boys!",
           "Big city snipes, boys!",
           "Silk mittens, top tittens!",
           "Fore check, back check, pay check, boys!",
           "Ferda.",
-          "If you’ve got a problem with her, then you’ve got a problem with me! And I suggest you let that one marinate!",
+          "If you’ve got a problem with her, then you’ve got a problem with me! \nAnd I suggest you let that one marinate!",
           "Dirty fuckin' dangles, boys!",
           "More hands make less work.",
-          "Fuck Lemony Snicket, what a series of unfortunate events you fuckin' been through you ugly fuck",
+          "Fuck Lemony Snicket, what a series of unfortunate events you fuckin' \nbeen through you ugly fuck",
           "Hard no.",
           "End of the laneway. Don't come up the property.",
           "That’s a Texas-sized 10-4, good buddy!",
@@ -29,11 +29,11 @@ quotes = ["Fuck you, your lifes so fuckin' miserable I ran a 15k to raise awaren
           "I guess we gots to calls up the internet.",
           "It’s impolite to kiss and tell.",
           "Three things, I hit you, you hit the pavement, ambulance hits 60.",
-          "I made your mum so wet, Trudeau deployed a 24-hour infantry unit to stack sand bags around my bed!",
-          "We'll see what you're calling us after I bury a couple of bar-down wristers.", 
-          "Half-clapper top-cheddar, biscuits top-titties bardownski, always bardownski.",
-          "Pull out the gun, safety's off, fuckin safety off boys",
-          "A little 3-on-1-ski, a little 3-on-2-ski, home a high hard one? Hit the red light district."]
+          "I made your mum so wet, Trudeau deployed a 24-hour infantry unit to \nstack sand bags around my bed!",
+          "We'll see what you're calling us after I bury a couple of bar-down \nwristers.", 
+          "Half-clapper top-cheddar, biscuits top-titties bardownski, always \nbardownski.",
+          "Pull out the gun, safety's off, fuckin safety off boys.",
+          "A little 3-on-1-ski, a little 3-on-2-ski, home a high hard one? Hit \nthe red light district."]
 
 # for quote in quotes:
 #     print(quote)
